3e9f2bff9704f1ad299ecd0896ef778a210a4d10
afa001 <anderson.alarcon.afa@gmail.com>
2020-07-30 11:19:01 -0500

    se agregò los controladores, vistas  y filtro por nombre en Persona

---
+++ ASP_NET_EF/ASP_NET_EF/Controllers/BancoController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ASP_NET_EF.Models;
+
+namespace ASP_NET_EF.Controllers
+{
+    public class BancoController : Controller
+    {
+        private bdAspNetEntities db = new bdAspNetEntities();
+
+        // GET: Banco
+        public ActionResult Index()
+        {
+            var banco = db.Banco.Include(b => b.Persona);
+            return View(banco.ToList());
+        }
+
+        // GET: Banco/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Banco banco = db.Banco.Find(id);
+            if (banco == null)
+            {
+                return HttpNotFound();
+            }
+            return View(banco);
+        }
+
+        // GET: Banco/Create
+        public ActionResult Create()
+        {
+            ViewBag.personaId = new SelectList(db.Persona, "Id", "Nombres");
+            return View();
+        }
+
+        // POST: Banco/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Nombre,Pais,Ciudad,Direccion,personaId")] Banco banco)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Banco.Add(banco);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.personaId = new SelectList(db.Persona, "Id", "Nombres", banco.personaId);
+            return View(banco);
+        }
+
+        // GET: Banco/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Banco banco = db.Banco.Find(id);
+            if (banco == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.personaId = new SelectList(db.Persona, "Id", "Nombres", banco.personaId);
+            return View(banco);
+        }
+
+        // POST: Banco/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Nombre,Pais,Ciudad,Direccion,personaId")] Banco banco)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(banco).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.personaId = new SelectList(db.Persona, "Id", "Nombres", banco.personaId);
+            return View(banco);
+        }
+
+        // GET: Banco/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Banco banco = db.Banco.Find(id);
+            if (banco == null)
+            {
+                return HttpNotFound();
+            }
+            return View(banco);
+        }
+
+        // POST: Banco/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Banco banco = db.Banco.Find(id);
+            db.Banco.Remove(banco);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ ASP_NET_EF/ASP_NET_EF/Controllers/PersonaController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ASP_NET_EF.Models;
+
+namespace ASP_NET_EF.Controllers
+{
+    public class PersonaController : Controller
+    {
+        private bdAspNetEntities db = new bdAspNetEntities();
+
+        // GET: Persona
+        public ActionResult Index(string nombre)
+        {
+            var personas = from p in db.Persona select p;
+
+            //filter nombre
+            if (!String.IsNullOrEmpty(nombre))
+            {
+                personas = personas.Where(p => p.Nombres.Contains(nombre));
+            }
+
+            return View(personas);
+            //return View(db.Persona.ToList());
+        }
+
+        // GET: Persona/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Persona persona = db.Persona.Find(id);
+            if (persona == null)
+            {
+                return HttpNotFound();
+            }
+            return View(persona);
+        }
+
+        // GET: Persona/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Persona/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Nombres,Apellidos,Edad,CorreoElectronico,Sexo,FechaNacimiento")] Persona persona)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Persona.Add(persona);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(persona);
+        }
+
+        // GET: Persona/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Persona persona = db.Persona.Find(id);
+            if (persona == null)
+            {
+                return HttpNotFound();
+            }
+            return View(persona);
+        }
+
+        // POST: Persona/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Nombres,Apellidos,Edad,CorreoElectronico,Sexo,FechaNacimiento")] Persona persona)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(persona).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(persona);
+        }
+
+        // GET: Persona/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Persona persona = db.Persona.Find(id);
+            if (persona == null)
+            {
+                return HttpNotFound();
+            }
+            return View(persona);
+        }
+
+        // POST: Persona/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Persona persona = db.Persona.Find(id);
+            db.Persona.Remove(persona);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
