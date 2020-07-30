583e1b7667a81025366034a5692eb589687e1e9d
afa001 <anderson.alarcon.afa@gmail.com>
2020-07-30 12:10:01 -0500

    se agregaron los controladores de web api y se configuro global.asax.cs

---
+++ API_EF/API_EF/Controllers/EquipoController.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using API_EF.Models;
+
+namespace API_EF.Controllers
+{
+    public class EquipoController : ApiController
+    {
+        private bdApiEfEntities db = new bdApiEfEntities();
+
+        // GET: api/Equipo
+        public IQueryable<Equipo> GetEquipo()
+        {
+            return db.Equipo;
+        }
+
+        // GET: api/Equipo/5
+        [ResponseType(typeof(Equipo))]
+        public IHttpActionResult GetEquipo(int id)
+        {
+            Equipo equipo = db.Equipo.Find(id);
+            if (equipo == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(equipo);
+        }
+
+        // PUT: api/Equipo/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutEquipo(int id, Equipo equipo)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != equipo.Id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(equipo).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!EquipoExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Equipo
+        [ResponseType(typeof(Equipo))]
+        public IHttpActionResult PostEquipo(Equipo equipo)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Equipo.Add(equipo);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = equipo.Id }, equipo);
+        }
+
+        // DELETE: api/Equipo/5
+        [ResponseType(typeof(Equipo))]
+        public IHttpActionResult DeleteEquipo(int id)
+        {
+            Equipo equipo = db.Equipo.Find(id);
+            if (equipo == null)
+            {
+                return NotFound();
+            }
+
+            db.Equipo.Remove(equipo);
+            db.SaveChanges();
+
+            return Ok(equipo);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool EquipoExists(int id)
+        {
+            return db.Equipo.Count(e => e.Id == id) > 0;
+        }
+    }
+}
---
+++ API_EF/API_EF/Controllers/JugadorController.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using API_EF.Models;
+
+namespace API_EF.Controllers
+{
+    public class JugadorController : ApiController
+    {
+        private bdApiEfEntities db = new bdApiEfEntities();
+
+        // GET: api/Jugador
+        public IQueryable<Jugador> GetJugador()
+        {
+            return db.Jugador;
+        }
+
+        // GET: api/Jugador/5
+        [ResponseType(typeof(Jugador))]
+        public IHttpActionResult GetJugador(int id)
+        {
+            Jugador jugador = db.Jugador.Find(id);
+            if (jugador == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(jugador);
+        }
+
+        // PUT: api/Jugador/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutJugador(int id, Jugador jugador)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != jugador.Id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(jugador).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!JugadorExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Jugador
+        [ResponseType(typeof(Jugador))]
+        public IHttpActionResult PostJugador(Jugador jugador)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Jugador.Add(jugador);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = jugador.Id }, jugador);
+        }
+
+        // DELETE: api/Jugador/5
+        [ResponseType(typeof(Jugador))]
+        public IHttpActionResult DeleteJugador(int id)
+        {
+            Jugador jugador = db.Jugador.Find(id);
+            if (jugador == null)
+            {
+                return NotFound();
+            }
+
+            db.Jugador.Remove(jugador);
+            db.SaveChanges();
+
+            return Ok(jugador);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool JugadorExists(int id)
+        {
+            return db.Jugador.Count(e => e.Id == id) > 0;
+        }
+    }
+}
